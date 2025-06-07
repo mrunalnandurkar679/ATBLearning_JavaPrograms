bf9d07abc06ed9bac8763461bc65e37d50222035
mrunalnandurkar679 <mrunal.nandurkar@gmail.com>
2025-06-07 17:12:11 +0530

    commit the calculator operation for division

--- src/main/java/Package_Scanner/LAB04_Calculator_04.java
+++ src/main/java/Package_Scanner/LAB04_Calculator_04.java
@@ -34,6 +34,17 @@ public class LAB04_Calculator_04 {
         System.out.println(result);
     }
 
+    public double division(int num1, int num2)
+    {
+        if (num2 ==0)
+        {
+            throw new ArithmeticException(" num is not divisible by 0");
+        }
+        result = num1/num2;
+        lastOperation = "division";
+        return result;
+    }
+
     public  String getLastOperation()
     {
         return lastOperation;
