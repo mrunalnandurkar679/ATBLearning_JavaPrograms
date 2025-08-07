56c8cbd41743979ec6d59e34f54758df5a897778
mrunalnandurkar679 <mrunal.nandurkar@gmail.com>
2025-08-07 21:17:33 +0530

    adding classes

--- src/main/java/Assasement_Java/Vehicle_Inheritance_Hierarchy.java
+++ src/main/java/Assasement_Java/Vehicle_Inheritance_Hierarchy.java
@@ -1,4 +1,41 @@
 package Assasement_Java;
 
 public class Vehicle_Inheritance_Hierarchy {
+    public static void main(String[] args) {
+       Vehicle car = new Car();
+       Vehicle bike = new Bike();
+       car.start(); bike.start();
+
+
+    }
 }
+
+class Vehicle
+{
+    void start()
+    {
+        System.out.println("Vehicle engine helps to start");
+    }
+}
+
+class Car extends  Vehicle
+{
+
+    void start()
+    {
+        System.out.println("Car engine starts with a roar!");
+    }
+}
+
+
+class Bike extends Vehicle
+{
+
+    void start()
+    {
+        System.out.println("Bike engine starts with a purr!");
+    }
+
+}
+
+
